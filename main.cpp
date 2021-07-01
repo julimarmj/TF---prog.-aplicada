44b8d18ef9a8fa5cad59d7f4205fbfffe2f95f66
Julimar Mees <82725291+julimarmj@users.noreply.github.com>
2021-07-01 00:57:56 +0000

    organizei um pouco pq tava impossivel entender.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 class no{
 public:
-  int valor;
+  float valor;
   no* proximo;
 
-  no(int elemento){
+  no(float elemento){
     this -> valor=elemento;
     this -> proximo=NULL;
   }
@@ -16,7 +16,7 @@ public:
     proximo=end;
   }
 
-  int obterValor(){
+  float obterValor(){
     return valor;
   }
 
@@ -25,17 +25,19 @@ public:
   }
 };
 
-no* inicio;
-no* fim;
+//======================================LISTA======================================
+
 class lista{
  public: 
-  
+  no* inicio;
+  no* fim;
+
   lista(){
     inicio=NULL;
     fim = NULL;
   }
 
-  lista(int num){
+  lista(float num){
     inicio = new no(num);
     fim = inicio;
   }
@@ -48,33 +50,7 @@ class lista{
     return (inicio == NULL);
   }
 
-  /*void imprimirElem(){
-    cout << "Elementos da lista" << endl;
-    no* i=inicio;
-    if (seVazia()){
-      cout << "lista sem elementos" << endl;
-    }else{
-      while (i){
-        cout << i -> obterValor() << endl;
-        i=i -> obterProximo();
-      }
-      cout << endl;
-    }
-  }*/
-
-  void insereNoInicio(int valor){
-    no* novo = new no(valor);
-
-    if (seVazia()){
-      inicio = novo;
-      fim = novo;
-    }else {
-      novo -> defProximo(inicio);
-      inicio = novo;
-    }
-  }
-
-  void insereNoFim(int valor){
+  void insereNoFim(float valor){
     no* novo = new no(valor);
 
     if (seVazia()){
@@ -85,147 +61,100 @@ class lista{
       fim = novo;
     }
   }
-  int tamanhoLista(){
-    if (seVazia()){
-      return 0;
-    }
- 
-    no* i = inicio;
-    int tamanho = 0;
 
-    do{
-      i = i -> obterProximo();
-      tamanho++;
-    }while(i);
-
-      return tamanho;
-    
-
-  };
-
-  bool existeElemento (int valor){
-    no* i = inicio;
-    while(i){
-      if(i -> obterValor() == valor){
-        return true;
-      }else{
-        i = i -> obterProximo();
-      }
-    }
-    return false;
-  }
-
-  void removerDaLista(){
-    if (!seVazia()){
-      if (inicio -> obterProximo() == NULL){
-        inicio = NULL;
-      }else if(inicio -> obterProximo() == NULL){
-        inicio -> defProximo(NULL);
-      }else{
-        no* anteriorDoAnterior = inicio;
-        no* anterior = inicio -> obterProximo();
-        no* elo = inicio -> obterProximo() -> obterProximo();
-
-        while (elo){
-          no* auxiliar = anterior;
-          anterior = elo;
-          anteriorDoAnterior = auxiliar;
-          elo = elo -> obterProximo();
-        }
-        delete anteriorDoAnterior -> obterProximo();
-        anteriorDoAnterior -> defProximo(NULL);
-        fim = anteriorDoAnterior;
-      }
-    }
-  }
-
-};
-
-  void paralelo(float v1, float r1, float r2){
-    float resT, corrente, potencia;
-    resT = r1*r2/(r1+r2);
-    corrente = v1/resT;
-    potencia = v1 * corrente;
-    cout << "\nA resistencia em paralelo é: " << resT << " ohm(s)\n";
-    cout << "A corrente total é: " << corrente <<" A\n";
-    cout << "A potencia da fonte é: " << potencia << " W\n";
-  }
-
-  void serie(float v1, float r1, float r2){
-    float resT, corrente, potencia;
-    resT = r1+r2;
-    corrente = v1/resT;
-    potencia = v1 * corrente;
-    cout << "\nA resistência em serie é: " << resT << " ohm(s)\n";
-    cout << "A corrente total é: " << corrente <<" A\n";
-    cout << "A potência da fonte é: " << potencia << " W\n";
-  }
-
-  void gravarNaLista(){
+   void gravarNaLista(string x){
     ofstream gravar;
     no* i=inicio;
-    gravar.open("Entradas.txt", ios::out);
+    gravar.open(x, ios::out);
       while (i){
         gravar << i -> obterValor() << endl;
         i=i -> obterProximo();
       }
-      gravar << "------------------";
+      gravar << "-----";
       gravar.close();
   }
+};
 
+//=====================================FUNÇÕES=====================================
+  
+  void paralelo(float v1, float r1, float r2){
+    lista out;
+    float resT, corrente, potencia;
+    resT = r1*r2/(r1+r2);
+    out.insereNoFim(resT);
+    corrente = v1/ resT;
+    out.insereNoFim(corrente);
+    potencia = v1 * corrente;
+    out.insereNoFim(potencia);
+    out.gravarNaLista("saidas.txt");
+  }
+
+  void serie(float v1, float r1, float r2){
+    lista out;
+    float resT, corrente, potencia;
+    resT = r1*r2;
+    out.insereNoFim(resT);
+    corrente = v1/ resT;
+    out.insereNoFim(corrente);
+    potencia = v1 * corrente;
+    out.insereNoFim(potencia);
+    out.gravarNaLista("saidas.txt");
+  }
+
+//=====================================MAIN=====================================
 
 int main() {
-  int i=0, x=1;
+  int i=0, tipo=0, x=1;
   lista var;
-  ifstream ler;
-  string texto = " ";
+  ifstream lerin, lerout;
+  string txt1 = " ", txt2 = " ";
 
   while (x!=0){
+    float v1=0, r1=0, r2=0;
     cout << "Escolha a ação a ser realizada: \n1 - Inserir dados\n2 - Ler dados\n0 - Sair\n";
     cin >> x;
 
     if (x == 1){
       cout << "Informe o valor da fonte: \n";
-      cin >> i;
-      var.insereNoFim(i);
+      cin >> v1;
+      var.insereNoFim(v1);
       cout << "Informe o valor da primeira resistência: \n";
-      cin >> i;
-      var.insereNoFim(i);
+      cin >> r1;
+      var.insereNoFim(r1);
       cout << "Informe o valor da segunda resistência: \n";
-      cin >> i;
-      var.insereNoFim(i);
-      gravarNaLista();
-
-    }else if(x == 2){
-      ler.open("Entradas.txt", ios::in);
-      cout << "------------------\n";
-      while (!ler.eof()){
-        ler >> texto;
-        cout << texto << endl;
-      }
-    ler.close();
-    }
-
-
-    /*do {
+      cin >> r2;
+      var.insereNoFim(r2);
+      var.gravarNaLista("Entradas.txt");
+      do {
       cout << "Para qual circuito deseja obter as informações: \n1 - Paralelo \n2 - Série \n";
       cin >> tipo;
 
-    if (tipo==1){
+       if (tipo==1){
         cout <<"------------------------------------------------\n";
-        cout << var;
-        paralelo( var.v1, var.r1, var.r2);
+        paralelo(v1, r1, r2);
         i=0;
         }else if(tipo == 2){
         cout <<"------------------------------------------------\n";
-        cout << var;
-        serie( var.v1, var.r1, var.r2);
+        serie(v1, r1, r2);
         i=0;
         }else {
         cout << "Opção nao listada!\n";
         i=1;
         }
     }    
-    while(i);*/
+    while(i);
+
+    }else if(x == 2){
+      lerin.open("Entradas.txt", ios::in);
+      lerout.open("saidas.txt", ios::in);
+      cout << "-----   -----\n";
+      while (!lerin.eof()){
+        lerin >> txt1;
+        lerout >> txt2;
+        cout << txt1 << "   " << txt2 << endl;
+      }
+    lerin.close();
+    lerout.close();
+    }
   }
 }
